3e308870815e03822022c83b9f18f2a0966299fb
can-solutions <116510846+can-solutions@users.noreply.github.com>
2022-11-27 15:28:50 +0200

    working with sqlite

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-import connexion
+# import connexion
 from flask import render_template
+import config
+from models import Person
 
-app = connexion.App(__name__, specification_dir="./")
-app.add_api("swagger.yml")
+# app = connexion.App(__name__, specification_dir="./")
+# app.add_api("swagger.yml")
+
+app = config.connex_app
+app.add_api(config.basedir / "swagger.yml")
 
 
+# @app.route("/")
+# def home():
+#     return render_template("home.html")
 @app.route("/")
 def home():
-    return render_template("home.html")
+    people = Person.query.all()
+    return render_template("home.html", people=people)
 
 
 if __name__ == "__main__":
